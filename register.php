56476ecd14f7f06d121f260b047bb8d282bb50d5
tabassumshifa <tabassumshifa.aiub23@gmail.com>
2026-05-04 20:00:44 +0600

    Registration page

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+include "db.php";
+
+if (isset($_POST['register'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+    
+    if ($conn->query($sql)) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+?>
+
+<form method="POST">
+    Name: <input type="text" name="name" required><br>
+    Email: <input type="email" name="email" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit" name="register">Register</button>
+</form>
